ea848fcac909f87f6e9dadb81ecbdfc08212d776
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2016-12-08 16:17:50 -0500

    removing cruft, adding better gulp pipeline, getting design started.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var shell = require('gulp-shell')
+var concat = require('gulp-concat');
 
 var sass = require('gulp-sass');
 
 
 var paths = {
-	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
-
-,
+	'src': ['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json'],
 	'style': {
-		all: './public/styles/**/*.scss',
+		all: ['./src/styles/**/*.scss'],
 		output: './public/styles/'
+	},
+	'js': {
+		all: ['./src/js/**/*.js'],
+		output: './public/js/'
 	}
-
 };
 
+gulp.task('watch:js', function() {
+	gulp.watch(paths.js.all, ['combinejs']);
+});
+gulp.task('combinejs', function() {
+	return gulp.src(paths.js.all)
+		.pipe(concat('build.js'))
+		.pipe(gulp.dest(paths.js.output));
+});
 
-gulp.task('watch:sass', function () {
+
+gulp.task('watch:sass', function() {
 	gulp.watch(paths.style.all, ['sass']);
 });
 
@@ -30,9 +41,8 @@ gulp.task('sass', function(){
 
 gulp.task('runKeystone', shell.task('node keystone.js'));
 gulp.task('watch', [
-
   'watch:sass',
-
+  'watch:js',
 ]);
 
 gulp.task('default', ['watch', 'runKeystone']);
