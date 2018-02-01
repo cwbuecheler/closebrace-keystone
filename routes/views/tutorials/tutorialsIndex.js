41c7d6baa7dc5309ba3a187750d1e8840502dca1
Christopher Buecheler <cwbuecheler@gmail.com>
2018-02-01 15:31:59 -0500

    removing unused code

--- routes/views/tutorials/tutorialsIndex.js
+++ routes/views/tutorials/tutorialsIndex.js
@@ -27,44 +27,6 @@ exports = module.exports = (req, res) => {
     });
   });
 
-  // ===================================================== //
-
-  // Load requested posts
-  view.on('init', function(next) {
-    var q = Post.model.find({
-      state: 'published',
-      postType: 'Tutorial',
-      hideFromIndex: false,
-    })
-    .sort('-publishedAt')
-    .populate('author categories');
-
-    q.exec(function(err, result) {
-
-      // If no posts, move along
-      if(result.length < 1) {
-        locals.firstPost = null;
-        locals.posts = null;
-        return next(err);
-      }
-
-      locals.firstPost = result[0];
-      locals.firstPost.updatedAtFormatted = locals.firstPost._.updatedAt.format('Do MMM YYYY');
-      locals.firstPost.publishedAtFormatted = locals.firstPost._.publishedAt.format('YYYY-MM-DD');
-      result.splice(0,1);
-      locals.posts = result;
-      for (var post in locals.posts) {
-        var updatedAtFormatted = locals.posts[post]._.updatedAt.format('Do MMM YYYY');
-        locals.posts[post].updatedAtFormatted = updatedAtFormatted;
-        var publishedAtFormatted = locals.posts[post]._.publishedAt.format('YYYY-MM-DD');
-        locals.posts[post].publishedAtFormatted = publishedAtFormatted;
-      }
-      next(err);
-    });
-  });
-
-  // ===================================================== //
-
   // Render the view
   view.render('tutorials/tutorialsIndex');
 };
