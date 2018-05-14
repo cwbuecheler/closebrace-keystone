7c86d34f05d9e4913dccfca9dbfebbf5d6a8a675
Christopher Buecheler <cwbuecheler@gmail.com>
2018-05-14 08:15:13 -0400

    cleaning up the index route

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,89 +1,15 @@
-var keystone = require('keystone');
-var FeedParser = require('feedparser');
-var request = require('request');
-var Post = keystone.list('Post');
+const keystone = require('keystone');
 
-exports = module.exports = function (req, res) {
+module.exports = (req, res) => {
+  const view = new keystone.View(req, res);
+  const locals = res.locals;
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'home';
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
-
-  // Get blog posts
-  view.on('init', function(next) {
-    locals.blogPosts = [];
-    var feed = request('http://blog.closebrace.com/rss/')
-    var feedparser = new FeedParser();
-
-    feed.on('error', function (error) {
-      // We'll generate an error on the front-end if needed
-      next();
-    });
-
-    feed.on('response', function (res) {
-      var stream = this;
-      if (res.statusCode !== 200) {
-        this.emit('error', new Error('Bad status code'));
-        next();
-      }
-      else {
-        stream.pipe(feedparser);
-      }
-    });
-
-    feedparser.on('error', function (error) {
-      // same deal, if there's an error, just bail out
-      next();
-    });
-
-    feedparser.on('readable', function () {
-      var stream = this;
-      var meta = this.meta;
-      var item;
-      while (item = stream.read()) {
-        if (locals.blogPosts.length < 5) {
-          item.summary = item.summary.replace('<p>','');
-          item.summary = item.summary.replace('</p>','');
-          item.summary = item.summary.substring(0, 100);
-          locals.blogPosts.push(item);
-        }
-      }
-    });
-    // move on the next step when the parser's done
-    feedparser.on('end', next);
-  });
-
-  // Load requested posts
-  view.on('init', function(next) {
-    var q = Post.model.find({
-      state: 'published',
-      hideFromIndex: false,
-    })
-    .sort({'publishedAt': -1})
-    .limit(12)
-    .populate('author categories');
-
-    q.exec(function(err, result) {
-      locals.posts = result;
-      locals.topTwo = [result[0], result[1]];
-      locals.nextFour = [result[2], result[3], result[4], result[5]];
-      locals.otherPosts = [];
-      for (var i = 6; i < result.length; i++) {
-        locals.otherPosts.push(result[i]);
-      }
-      for (var post in result) {
-        var updatedAtFormatted = result[post]._.updatedAt.format('Do MMM YYYY');
-        locals.posts[post].updatedAtFormatted = updatedAtFormatted;
-        var publishedAtFormatted = locals.posts[post]._.publishedAt.format('YYYY-MM-DD');
-        locals.posts[post].publishedAtFormatted = publishedAtFormatted;
-      }
-      next(err);
-    });
-  });
-
-	// Render the view
-	view.render('index', { layout: 'wide' });
+  // Render the view
+  view.render('index', { layout: 'wide' });
 };
+
+exports = module.exports;
